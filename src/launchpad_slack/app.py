1162e9d8831f0d38ee7d785c198f14770f9e8be7
Aearsis <Hlavaty.Ondrej@gmail.com>
2017-06-16 00:09:32 +0200

    Just testing something

--- src/launchpad_slack/app.py
+++ src/launchpad_slack/app.py
@@ -12,14 +12,7 @@ app = Flask(__name__)
 
 ALLOWED_EVENTS = [
     'bzr:push:0.1',
-    'git:push:0.1',
-    'merge-proposal:0.1',
 ]
-
-COLOR_RED = '#e74c3c'
-COLOR_GREEN = '#2ecc71'
-COLOR_BLUE = '#3498db'
-COLOR_YELLOW = '#f1c40f'
 
 
 @app.route('/webhook', methods=['post'])
@@ -32,87 +25,15 @@ def webhook():
         icon_url = settings.SLACK_ICON_URL
         channel = settings.SLACK_CHANNEL
 
-        commit_message = None
-        description = None
-        old_queue_status = None
-        color = COLOR_YELLOW
-
-        fields = []
+        text = None
 
         if lp_event == 'bzr:push:0.1':
-            pretext = "New bzr commit has been pushed"
-            title_link = "http://bazaar.launchpad.net/%s/revision/%s" % (
+            link = "http://bazaar.launchpad.net/%s/revision/%s" % (
                 request.json['bzr_branch_path'], request.json['new']['revision_id'])
-
-        if lp_event == 'git:push:0.1':
-            pretext = "New git commit has been pushed"
-            title_link = "https://git.launchpad.net/%s/commit/?id=%s" % (
-                request.json['git_repository'], request.json['commit_sha1'])
-
-        if lp_event == 'merge-proposal:0.1':
-            merge_proposal = request.json['merge_proposal']
-            action = request.json['action']
-
-            registrant = request.json['new']['registrant']
-            description = request.json['new']['description']
-            commit_message = request.json['new']['commit_message']
-
-            try:
-                old_queue_status = request.json['old']['queue_status']
-            except KeyError:
-                pass
-            new_queue_status = request.json['new']['queue_status']
-
-            if old_queue_status:
-                status = u"%s → %s" % (old_queue_status, new_queue_status)
-            else:
-                status = u"%s" % new_queue_status
-
-            fields.append({
-                'title': 'Status',
-                'value': status,
-                'short': True,
-            })
-
-            fields.append({
-                'title': 'Author',
-                'value': "<%s|%s>" % (registrant, registrant[2:]),
-                'short': True,
-            })
-
-            if action == "created":
-                pretext = "Merge request has been proposed"
-                color = COLOR_BLUE
-            if action == "modified":
-                if new_queue_status == "Work in progress":
-                    pretext = "Merge request has been marked as WIP"
-                    color = COLOR_BLUE
-                if new_queue_status == "Needs review":
-                    pretext = "Merge request has been updated"
-                    color = COLOR_YELLOW
-                if new_queue_status == "Approved":
-                    pretext = "Merge request has been approved"
-                    color = COLOR_GREEN
-                if new_queue_status == "Merged":
-                    pretext = "Branch has been merged"
-                    color = COLOR_GREEN
-                if new_queue_status == "Rejected":
-                    pretext = "Merge request has been rejected"
-                    color = COLOR_RED
-
-            title_link = "https://code.launchpad.net%s" % merge_proposal
+            text = "New <%s|commit> has been pushed!" % (link, )
 
         payload = {
-            'parse': 'none',
-            'attachments': [{
-                'fallback': u'%s' % pretext,
-                'pretext': u'%s' % pretext,
-                'title': commit_message or description or None,
-                'title_link': title_link,
-                'text': "%s" % description or None,
-                'color': color,
-                'fields': fields,
-            }]
+            'text': text
         }
 
         if username:
@@ -212,7 +133,7 @@ def lp():
 
 @app.route('/')
 def index():
-    return redirect('https://github.com/daker/launchpad-slack')
+    return redirect('https://github.com/mff-dungeon/launchpad-slack')
 
 
 if __name__ == '__main__':
